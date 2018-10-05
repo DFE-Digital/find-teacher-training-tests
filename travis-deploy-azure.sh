7ad688bfcaa890afa5562784ecdda80fed64d50d
Theodor Vararu <theo@vararu.org>
2018-10-05 14:54:51 +0100

    Update deploy script, create deploy folder
    This is to prevent zipping up unwanted files, and also to get Azure to recognise the project as a nodejs one.

--- travis-deploy-azure.sh
+++ travis-deploy-azure.sh
@@ -2,11 +2,23 @@
 
 deployZip=deploy.zip
 
+echo "Creating deploy folder…"
+mkdir deploy
+
+echo "Moving tests to deploy folder…"
+cp -r cypress deploy
+cp -r node_modules deploy
+cp cypress.json deploy
+cp package-lock.json deploy
+cp package.json deploy
+cp settings.job deploy
+cp webjob.js deploy
+
 echo "Removing old $deployZip if any…"
 [ -e $deployZip ] && rm $deployZip
 
 echo "Creating zip…"
-zip $deployZip -r ./
+zip $deployZip -r ./deploy
 
 echo "Uploading to azure WebJob…"
 curl -X PUT -u "$1" --data-binary @$deployZip --header "Content-Type: application/zip" --header "Content-Disposition: attachment; filename=$deployZip" https://$2.scm.azurewebsites.net/api/triggeredwebjobs/smoke-tests/
