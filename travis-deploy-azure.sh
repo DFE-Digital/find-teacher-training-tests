79c24e5b27d531c40b677aba5bf6bf2495fa4e7c
Theodor Vararu <theo@vararu.org>
2018-10-05 13:12:50 +0100

    Add Travis script to deploy webjob to Azure

---
+++ travis-deploy-azure.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+deployZip=deploy.zip
+
+echo "Removing old $deployZip if any..."
+[ -e $deployZip ] && rm $deployZip
+
+echo "Creating zip…"
+zip $deployZip -r ./
+
+echo "Uploading to azure WebJob…"
+curl -X PUT -u "$1" --data-binary @$deployZip --header "Content-Type: application/zip" --header "Content-Disposition: attachment; filename=$deployZip" https://$2.scm.azurewebsites.net/api/triggeredwebjobs/smoke-tests/
+
+echo "Deploy complete."
