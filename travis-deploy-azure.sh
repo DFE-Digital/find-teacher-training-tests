903f32ebccf2c33ba0c71aee5693d033eabb2d88
Theodor Vararu <theo@vararu.org>
2018-10-05 15:18:41 +0100

    Make deploy zipping quiet so that logs actually appear on travis

--- travis-deploy-azure.sh
+++ travis-deploy-azure.sh
@@ -18,7 +18,7 @@ echo "Removing old $deployZip if any…"
 [ -e $deployZip ] && rm $deployZip
 
 echo "Creating zip…"
-zip $deployZip -r ./deploy
+zip --quiet $deployZip -r ./deploy
 
 echo "Uploading to azure WebJob…"
 curl -X PUT -u "$1" --data-binary @$deployZip --header "Content-Type: application/zip" --header "Content-Disposition: attachment; filename=$deployZip" https://$2.scm.azurewebsites.net/api/triggeredwebjobs/smoke-tests/
