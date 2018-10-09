816d338d5d3de11db7fbac54167bce59fc4bebe9
Theodor Vararu <theo@vararu.org>
2018-10-09 15:33:14 +0100

    Sprinkle in logging while debugging the Azure webjob

--- webjob.js
+++ webjob.js
@@ -1,3 +1,4 @@
+console.log("1. Foo");
 const appInsights = require("applicationinsights");
 
 appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY);
@@ -5,7 +6,11 @@ appInsights.start();
 
 const client = appInsights.defaultClient;
 
+console.log("2. Bar");
+
 client.trackEvent({
   name: "Smoke tests working!",
   properties: { customProperty: "No tests to run yet, but that will change soon" }
 });
+
+console.log("3. End");
