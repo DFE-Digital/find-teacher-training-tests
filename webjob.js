8ee2d95b0119433673ab488016f9642f252dbf5b
Theodor Vararu <theo@vararu.org>
2018-10-09 15:35:05 +0100

    Logs not necessary anymore because the webjob works

--- webjob.js
+++ webjob.js
@@ -1,4 +1,3 @@
-console.log("1. Foo");
 const appInsights = require("applicationinsights");
 
 appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY);
@@ -6,11 +5,7 @@ appInsights.start();
 
 const client = appInsights.defaultClient;
 
-console.log("2. Bar");
-
 client.trackEvent({
   name: "Smoke tests working!",
   properties: { customProperty: "No tests to run yet, but that will change soon" }
 });
-
-console.log("3. End");
