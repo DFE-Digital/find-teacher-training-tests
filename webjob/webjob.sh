5e866c08e55fd67adab66e55e401a4dd43970db6
Theodor Vararu <theo@vararu.org>
2018-10-19 16:37:13 +0100

    Re-enable tests to run on all environments

--- webjob/webjob.sh
+++ webjob/webjob.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+# Dev
 curl -s -X POST \
     -H "Content-Type: application/json" \
     -H "Accept: application/json" \
@@ -7,3 +8,21 @@ curl -s -X POST \
     -H "Authorization: token $TRAVIS_KEY" \
     -d "{ \"request\": { \"branch\": \"master\" } }" \
     https://api.travis-ci.org/repo/DFE-Digital%2fsearch-and-compare-ui-tests/requests
+
+# Staging
+curl -s -X POST \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -H "Travis-API-Version: 3" \
+    -H "Authorization: token $TRAVIS_KEY" \
+    -d "{ \"request\": { \"branch\": \"staging\" } }" \
+    https://api.travis-ci.org/repo/DFE-Digital%2fsearch-and-compare-ui-tests/
+
+# Live
+curl -s -X POST \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -H "Travis-API-Version: 3" \
+    -H "Authorization: token $TRAVIS_KEY" \
+    -d "{ \"request\": { \"branch\": \"production\" } }" \
+    https://api.travis-ci.org/repo/DFE-Digital%2fsearch-and-compare-ui-tests/
