1b57dedd45596b5fed9d95fe368fa3261e3c7d77
Theodor Vararu <theo@vararu.org>
2018-10-19 16:01:24 +0100

    Remove multiple branches from webjob script

--- webjob/webjob.sh
+++ webjob/webjob.sh
@@ -1,28 +1,9 @@
 #!/usr/bin/env sh
 
-# Dev
 curl -s -X POST \
     -H "Content-Type: application/json" \
     -H "Accept: application/json" \
     -H "Travis-API-Version: 3" \
     -H "Authorization: token $TRAVIS_KEY" \
     -d "{ \"request\": { \"branch\": \"master\" } }" \
-    https://api.travis-ci.org/repo/DFE-Digital%2fsearch-and-compare-ui-tests/requests
-
-# Staging
-curl -s -X POST \
-    -H "Content-Type: application/json" \
-    -H "Accept: application/json" \
-    -H "Travis-API-Version: 3" \
-    -H "Authorization: token $TRAVIS_KEY" \
-    -d "{ \"request\": { \"branch\": \"staging\" } }" \
-    https://api.travis-ci.org/repo/DFE-Digital%2fsearch-and-compare-ui-tests/requests
-
-# Live
-curl -s -X POST \
-    -H "Content-Type: application/json" \
-    -H "Accept: application/json" \
-    -H "Travis-API-Version: 3" \
-    -H "Authorization: token $TRAVIS_KEY" \
-    -d "{ \"request\": { \"branch\": \"production\" } }" \
     https://api.travis-ci.org/repo/DFE-Digital%2fsearch-and-compare-ui-tests/requests
