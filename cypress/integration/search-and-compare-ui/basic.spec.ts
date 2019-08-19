03c99d5774f81dcd119de1f9da3bf822d6efcac6
Theodor Vararu <theo@vararu.org>
2019-08-19 16:11:37 +0100

    Update course title for basic test
    The previously specified course seems to have been taken off the site, so now our test breaks.
    
    This is the third time we have had to do this in a short span of time.

--- cypress/integration/search-and-compare-ui/basic.spec.ts
+++ cypress/integration/search-and-compare-ui/basic.spec.ts
@@ -39,7 +39,7 @@ describe("Basic", () => {
   });
 
   it("should let users view a course", () => {
-    const courseTitle = "Bath Spa University";
+    const courseTitle = "Bishop Grosseteste University";
     cy.contains(courseTitle).click({ force: true }); // Forcing because Cypress erroneously thinks the element can't be interacted with.
     cy.get("h1").should("contain", courseTitle);
   });
